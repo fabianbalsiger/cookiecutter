eaf7e8304b57dbb4740b5e77fc819e57dfe3aa9f
fabianbalsiger <accounts@fabianbalsiger.ch>
2023-02-27 11:52:25 +0100

    update test name

---
+++ tests/test_graspit_cookiecutter.py
@@ -0,0 +1,12 @@
+"""Barely functional tests for graspit-coefficient (not the project it generates).
+
+Usage:
+    pytest -s ./tests/test_graspit_cookiecutter.py
+"""
+
+from __future__ import annotations
+
+
+def test_not_much_at_all():
+    """This tests nothing."""
+    assert True
